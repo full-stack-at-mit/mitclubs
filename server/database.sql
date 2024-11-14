c311d3a3c8b658cb8278393bd6e56152c4f02796
Hailey <haileyrpan@gmail.com>
2024-11-14 10:50:51 -0500

    add clubs to database

--- server/database.sql
+++ server/database.sql
@@ -5,3 +5,18 @@ create table users(
     password varchar(255) not null,
     created_at date default current_date
 );
+
+-- clubs table
+create table clubs(
+    club_id serial primary key,
+    name varchar(255) not null,
+    is_active boolean not null,
+    is_accepting boolean not null,
+    recruiting_cycle varchar(255),
+    membership_process varchar(255),
+    type varchar(255),
+    email varchar(255),
+    website varchar(255),
+    mission text,
+    picture_url varchar(255)
+);
